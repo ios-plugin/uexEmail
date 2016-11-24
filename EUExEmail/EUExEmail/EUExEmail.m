b6b2e20c800b7e1f67b3c406be804a0256dba7e5
connyliu <21165491@qq.com>
2016-11-24 15:23:58 +0800

    Merge branch 'dev-4.0'

--- EUExEmail/EUExEmail/EUExEmail.m
+++ EUExEmail/EUExEmail/EUExEmail.m
@@ -13,49 +13,40 @@
 
 @implementation EUExEmail
 
--(id)initWithBrwView:(EBrowserView *) eInBrwView{
-	if (self = [super initWithBrwView:eInBrwView]) {
-	}
-	return self;
-}
 
 -(void)dealloc{
     mailObj = nil;
-
 }
 
 - (void)open:(NSMutableArray *)inArguments {
     if ([inArguments count] < 2) {
         return;
     }
-	NSString * inReceiverEmail = [inArguments objectAtIndex:0];
-	NSString * inSubject = [inArguments objectAtIndex:1];
+
+    NSString * inReceiverEmail = [inArguments objectAtIndex:0];
+    NSString * inSubject = [inArguments objectAtIndex:1];
     NSString * inContent = [inArguments count] > 2 ? [inArguments objectAtIndex:2] : @"";
     NSString * inAttachmentPath = [inArguments count] > 3 ?[inArguments objectAtIndex:3] : @"";
-	//跳转到发邮件应用，不能返回
-	//[[UIApplication sharedApplication] openURL:[NSURL URLWithString:[NSString stringWithFormat:@"mailto://%@",inReceiverEmail]]];
+    //跳转到发邮件应用，不能返回
+    //[[UIApplication sharedApplication] openURL:[NSURL URLWithString:[NSString stringWithFormat:@"mailto://%@",inReceiverEmail]]];
     //启动一个邮件发送界面，可以返回
-	if (inReceiverEmail == nil) {
-		[super jsFailedWithOpId:0 errorCode:1080101 errorDes:UEX_ERROR_DESCRIBE_ARGS];
-		return;
-	}
+
+    NSMutableDictionary * dict = [[NSMutableDictionary alloc] initWithCapacity:10];
     
-	NSMutableDictionary * dict = [[NSMutableDictionary alloc] initWithCapacity:10];
+    [dict setObject:inReceiverEmail forKey:@"receiver"];
+    [dict setObject:inSubject forKey:@"subject"];
+    [dict setObject:inContent forKey:@"content"];
+    [dict setObject:inAttachmentPath forKey:@"attachment"];
     
-	[dict setObject:inReceiverEmail forKey:@"receiver"];
-	[dict setObject:inSubject forKey:@"subject"];
-	[dict setObject:inContent forKey:@"content"];
-	[dict setObject:inAttachmentPath forKey:@"attachment"];
-    
-	if (!mailObj) {
-		mailObj = [[Email alloc] init];
+    if (!mailObj) {
+        mailObj = [[Email alloc] init];
     }
-	[mailObj openMailWithUExObj:self argDict:dict];
+    [mailObj openMailWithUExObj:self argDict:dict];
 }
 
-    
-    
-    
+
+
+
 - (void)clean{
     mailObj = nil;
 }
